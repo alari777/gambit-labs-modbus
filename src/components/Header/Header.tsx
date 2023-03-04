2b1302f13293565bb85b2085d769979dffac811f
alari777 <alari777@gmail.com>
2023-03-04 22:45:15 +0200

    updated `Header` component with links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,13 @@
 import { FC } from 'react';
 import styles from '@/components/Header/Header.module.scss';
 import Link from 'next/link';
+import { useUser } from '@auth0/nextjs-auth0/client';
 
 const Header: FC = () => {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
   return (
     <header className={styles.header}>
       <nav className='navbar navbar-expand-lg navbar-light'>
@@ -15,13 +20,33 @@ const Header: FC = () => {
             >
               Home
             </Link>
-            <Link
-              data-testid='nav-protection-page'
-              className='nav-item nav-link'
-              href='/protection-page'
-            >
-              Protection page (?)
-            </Link>
+            {user && (
+              <>
+                <Link
+                  data-testid='nav-protection-page'
+                  className='nav-item nav-link'
+                  href='/protection-page'
+                >
+                  Protection Home Page
+                </Link>
+                <Link
+                  data-testid='nav-logout'
+                  className='nav-item nav-link'
+                  href='/api/auth/logout'
+                >
+                  Logout ({user.name})
+                </Link>
+              </>
+            )}
+            {!user && (
+              <Link
+                data-testid='nav-login'
+                className='nav-item nav-link'
+                href='/api/auth/login'
+              >
+                Login
+              </Link>
+            )}
           </div>
         </div>
       </nav>
