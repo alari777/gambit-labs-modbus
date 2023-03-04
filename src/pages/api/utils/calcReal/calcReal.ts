2dd42a68c07eead630455a3dc692d61fea525a7d
alari777 <alari777@gmail.com>
2023-03-04 09:44:43 +0200

    created function in order to calculation `REAL4` value of the registers

---
+++ src/pages/api/utils/calcReal/calcReal.ts
@@ -0,0 +1,18 @@
+import { Modbus } from '@/pages/api/classes/Modbus.class';
+
+export function calcReal(index: number): number {
+  const modbus = Modbus.init();
+  // Create `arraybuffer` for 4 bytes.
+  // It equals 32 bits: 16 bits for one register and 16 bits for second register
+  const buffer = new ArrayBuffer(4);
+  const view = new DataView(buffer);
+  if (modbus.data) {
+    // Set the first value of register
+    view.setUint16(0, modbus.data[index], true);
+    // Set the second value of register
+    view.setUint16(2, modbus.data[index + 1], true);
+    //read the buffer as float and round to 5 digits
+    return parseFloat(view.getFloat32(0, true).toFixed(15));
+  }
+  return 0;
+}
