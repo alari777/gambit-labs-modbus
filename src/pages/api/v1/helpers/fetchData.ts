443e3242f9ec44b0b80fadd28f9d79a6d3948161
alari777 <alari777@gmail.com>
2023-03-04 16:45:07 +0200

    refactored function `fetchData`, was added try/catch block

--- src/pages/api/v1/helpers/fetchData.ts
+++ src/pages/api/v1/helpers/fetchData.ts
@@ -1,13 +1,18 @@
 // This function downloads remote text file with registers data
 export async function fetchData(): Promise<string> {
-  const response = await fetch('http://tuftuf.gambitlabs.fi/feed.txt', {
-    method: 'GET',
-    headers: {
-      'content-type': 'text/plain;charset=UTF-8',
-    },
-  });
-  if (response.status === 200) {
-    return await response.text();
+  try {
+    const response = await fetch('http://tuftuf.gambitlabs.fi/feed.txt', {
+      method: 'GET',
+      headers: {
+        'content-type': 'text/plain;charset=UTF-8',
+      },
+    });
+    if (response.status === 200) {
+      return await response.text();
+    } else {
+      return '';
+    }
+  } catch (err) {
+    return '';
   }
-  return '';
 }
